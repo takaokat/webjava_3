d59aeb565ea68a38165e3470e3e44997bc80f56f
systena <systena@192.168.11.28>
2020-03-08 19:54:12 +0900

    リポジトリ名を修正

--- src/test/java/jp/co/systena/tigerscave/kadai3/Kadai3ApplicationTests.java
+++ src/test/java/jp/co/systena/tigerscave/kadai3/Kadai3ApplicationTests.java
@@ -1,4 +1,4 @@
-package jp.co.systena.tigerscave.kadai2;
+package jp.co.systena.tigerscave.kadai3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
