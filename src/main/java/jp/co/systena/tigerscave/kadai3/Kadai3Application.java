70c4f051ee56f094d5d429a1a8a662a8038d34ce
systena <systena@192.168.11.28>
2020-03-08 19:46:48 +0900

    課題③提出

---
+++ src/main/java/jp/co/systena/tigerscave/kadai3/Kadai3Application.java
@@ -0,0 +1,13 @@
+package jp.co.systena.tigerscave.kadai3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kadai3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kadai3Application.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/systena/tigerscave/kadai2/Kadai3ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.systena.tigerscave.kadai2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kadai3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
